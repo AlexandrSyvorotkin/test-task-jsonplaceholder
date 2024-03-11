022cba77d0bbe362a2d6651958a99539dea2e69b
Alex <alex031994@mail.ru>
2024-03-11 15:37:37 +0300

    fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://alexandrsyvorotkin.github.io/test-task-jsonplaceholder/'
 })
