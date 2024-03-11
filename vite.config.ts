781e110944ffb588e76afeefebd18d4948cf739c
Alex <alex031994@mail.ru>
2024-03-11 12:49:02 +0300

    deploing

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://alexandrsyvorotkin.github.io/test-task-jsonplaceholder/'
 })
